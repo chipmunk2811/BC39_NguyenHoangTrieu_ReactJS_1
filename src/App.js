0e1fb7c164710b12cd8520c167a7a15d5e3ac837
chipmunk2811 <chipmunk2811@yahoo.com>
2022-12-18 13:04:28 +0700

    Baitap1

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import ReactBuoi1 from "./ReactBuoi1/index.js";
+
+export default function App() {
+  return (
+    <>
+      <ReactBuoi1 />
+    </>
+  );
+}
+
+
---
+++ src/ReactBuoi1/index.js
@@ -0,0 +1,17 @@
+import "../App.css"
+import Header from "./header"
+import Carousel from "./carousel";
+import Content from "./content"
+import Footer from "./footer"
+function ReactBuoi1() {
+    return (
+        <>
+            <Header />
+            <Carousel />
+            <Content />
+            <Footer />
+        </>
+    )
+}
+
+export default ReactBuoi1;
